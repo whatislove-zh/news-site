8ae734aab330bd9fb0211b13d68c278023c30fdf
Vladislav <vladgm233223@gmail.com>
2023-02-27 16:48:14 +0200

    create login page markup and style

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import { Button, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
 
 export const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Box
+      sx={{ display: "flex",flexDirection:"column", justifyContent: "center", alignItems: "center", height:"80vh" }}
+    >
+      <Typography variant="h5">Enter your username/email and password please</Typography>
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "50%",
+          margin:"25px"
+        }}
+      >
+        <TextField
+          fullWidth
+          autoComplete="email"
+          sx={{ m: "15px" }}
+          placeholder="Login"
+        />
+        <TextField
+          fullWidth
+          autoComplete="password"
+          type="password"
+          sx={{ m: "15px" }}
+          placeholder="Password"
+        />
+        <Button type="submit" variant="outlined" sx={{ width: "30%" }}>
+          Sign In
+        </Button>
+      </Box>
+    </Box>
+  );
+};
