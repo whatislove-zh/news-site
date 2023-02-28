627a4b9f86a467a9e84e662bdf4ddfcbeca9bd2d
Vladislav <vladgm233223@gmail.com>
2023-02-28 13:31:21 +0200

    add posts to a page

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import { Typography, Grid } from "@mui/material";
 
-export const News:React.FC = () => {
+import { useAppSelector, useAppDispatch } from "../store/hook";
+import {
+  selectPosts,
+  selectPostsInfo,
+} from "../store/features/getPosts/postsSlise";
+import { useEffect } from "react";
+import { loadPosts } from "../store/features/getPosts/postsSlise";
+import { PostCard } from "../components/PostCard";
+
+export const News: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const posts = useAppSelector(selectPosts);
+  const { qty, status, error } = useAppSelector(selectPostsInfo);
+
+  useEffect(() => {
+    if (!qty) {
+      dispatch(loadPosts());
+    }
+  }, [qty, dispatch]);
+
   return (
-    <div>News</div>
-  )
-}
+    <>
+      {status === "loading" && <Typography>Loading...</Typography>}
+      {error && <Typography>Error, my bad, sorry</Typography>}
+      {status === "received" && (
+        <Grid container spacing={2} sx={{ mt: "45px" }}>
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+        </Grid>
+      )}
+    </>
+  );
+};
