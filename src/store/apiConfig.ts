f21e239f2601a403a65e482b0f9b30e687e2f355
Vladislav <vladgm233223@gmail.com>
2023-02-28 13:25:35 +0200

    Create apiConfig.ts

---
+++ src/store/apiConfig.ts
@@ -0,0 +1,13 @@
+
+const BASE_URL: string = "https://api.spaceflightnewsapi.net/v3/articles"
+
+export const ALL_POSTS = BASE_URL + "?_limit=100"
+
+export const searchPostById = (id:string | number) => BASE_URL + `/${id}`
+
+type apiType = {
+    BASE_URL: string,
+    ALL_POSTS: string,
+    searchPostById:(id: string | number) => string
+}
+export default apiType
