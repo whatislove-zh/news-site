94004b19ef847606ea2e88039f16f773621c569a
Vladislav <vladgm233223@gmail.com>
2023-03-01 17:10:42 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,5 +2,5 @@ import { Typography } from "@mui/material";
 
 
 export const Footer = () => {
-  return <Typography align="center">All rights reserved</Typography>;
+  return <Typography align="center" sx={{m:"30px"}}>All rights reserved</Typography>;
 };
