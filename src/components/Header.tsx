50ee99adf81a55a27ecfe27fa50bd024730c8316
Vladislav <vladgm233223@gmail.com>
2023-03-01 17:11:09 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,21 @@
-import { AppBar,  Box, IconButton, Toolbar, Typography, Button } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Toolbar,
+  Typography,
+  Button,
+} from "@mui/material";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 
-
 import { Link } from "react-router-dom";
+import { useAppSelector } from "../store/hook";
 
 export const Header = () => {
+  const user = useAppSelector((state) => state.profile);
+  const isSignIn = user.isSignIn;
   return (
-    <AppBar position="static" sx={{ background: "none", mb:"30px" }}>
+    <AppBar position="static" sx={{ background: "none", mb: "30px" }}>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
         <Box>
           <IconButton size="large" edge="start" sx={{ color: "#000000" }}>
@@ -17,16 +26,22 @@ export const Header = () => {
           </IconButton>
         </Box>
         <Box>
-          
-          <Link to="/" style={{textDecoration:"none"}}>
+          <Link to="/" style={{ textDecoration: "none" }}>
             <Button>Home</Button>
           </Link>
-          <Link to="news" style={{textDecoration:"none"}}>
+          <Link to="news" style={{ textDecoration: "none" }}>
             <Button>News</Button>
           </Link>
-          <Link to="login" style={{textDecoration:"none"}}>
-            <Button>Login</Button>
-          </Link>
+
+          {isSignIn ? (
+            <Link to="profile" style={{ textDecoration: "none" }}>
+              <Button>Profile</Button>
+            </Link>
+          ) : (
+            <Link to="login" style={{ textDecoration: "none" }}>
+              <Button>Login</Button>
+            </Link>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
