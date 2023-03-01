c1372996e06d714b74d9f18905d8ba68b23300fb
Vladislav <vladgm233223@gmail.com>
2023-03-01 17:08:48 +0200

    make a logik for simple login and form validation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,67 @@
 import { Button, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { loginUser } from "../store/features/profleInfo/profileSlice";
+import { useAppDispatch, useAppSelector } from "../store/hook";
+import { formDataType } from "../types/types";
+import { useEffect } from "react";
+
 
 export const Login = () => {
+  const isUserSignIn = useAppSelector((state) => state.profile.isSignIn);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isUserSignIn) {
+      navigate("/profile");
+    }
+  }, [isUserSignIn, navigate]);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm<formDataType>({
+    mode: "onBlur",
+  });
+
+
+  const dispatch = useAppDispatch();
+
+  const onSubmit: SubmitHandler<formDataType> = (data) => {
+    dispatch(loginUser(data));
+    localStorage.setItem("user", JSON.stringify({ ...data, isSignIn: true }));
+
+    reset();
+    navigate("/profile");
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <Box
-      sx={{ display: "flex",flexDirection:"column", justifyContent: "center", alignItems: "center", minHeight:"80vh" }}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "75vh",
+      }}
     >
-      <Typography variant="h5">Enter your username/email and password please</Typography>
+      <Typography variant="h5">
+        Enter your username/email and password please
+      </Typography>
       <Box
         component="form"
+        onSubmit={handleSubmit(onSubmit)}
         sx={{
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
           width: "50%",
-          margin:"25px"
+          margin: "25px",
         }}
       >
         <TextField
@@ -23,6 +69,15 @@ export const Login = () => {
           autoComplete="email"
           sx={{ m: "15px" }}
           placeholder="Login"
+          helperText={errors?.login ? (errors.login.message as string) : ""}
+          error={errors?.login ? true : false}
+          {...register("login", {
+            required: "This field is required",
+            pattern: {
+              value: /admin/,
+              message: "Login is not correct",
+            },
+          })}
         />
         <TextField
           fullWidth
@@ -30,8 +85,24 @@ export const Login = () => {
           type="password"
           sx={{ m: "15px" }}
           placeholder="Password"
+          helperText={
+            errors?.password ? (errors.password.message as string) : ""
+          }
+          error={errors?.password ? true : false}
+          {...register("password", {
+            required: "This field is required",
+            pattern: {
+              value: /12345/,
+              message: "Password is correct",
+            },
+          })}
         />
-        <Button type="submit" variant="outlined" sx={{ width: "30%" }}>
+        <Button
+          disabled={!isValid}
+          type="submit"
+          variant="outlined"
+          sx={{ width: "30%", minWidth: "100px" }}
+        >
           Sign In
         </Button>
       </Box>
