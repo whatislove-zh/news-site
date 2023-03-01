99d18548e67046d252b4082feff402b0d0f0bcb9
Vladislav <vladgm233223@gmail.com>
2023-03-01 17:09:40 +0200

    remove loading posts to root File

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,22 @@ import { Login } from "./pages/Login";
 import { News } from "./pages/News";
 import { NotFound } from "./pages/NotFound";
 import { Profile } from "./pages/Profile";
+import {useEffect} from "react"
+import { loadPosts } from "./store/features/getPosts/postsSlise";
+import {  useAppDispatch, useAppSelector } from "./store/hook";
+import { selectPostsInfo } from "./store/features/getPosts/postsSlise";
 
 const App: React.FC = () => {
+
+  const dispatch = useAppDispatch();
+  const { qty } = useAppSelector(selectPostsInfo);
+
+  useEffect(() => {
+    if (!qty) {
+      dispatch(loadPosts());
+    }
+  }, [qty, dispatch]);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,22 +1,22 @@
 import { Typography, Grid, Button } from "@mui/material";
-import { useAppSelector, useAppDispatch } from "../store/hook";
+import { useAppSelector} from "../store/hook";
 import {
   selectPosts,
   selectPostsInfo,
 } from "../store/features/getPosts/postsSlise";
-import { useEffect, useState } from "react";
-import { loadPosts } from "../store/features/getPosts/postsSlise";
+import { useState } from "react";
+
 import { PostCard } from "../components/PostCard";
 
 export const News: React.FC = () => {
   const [numberOfitemsShown, setNumberOfItemsToShown] = useState(6);
   const [isOverflowList, setIsOverflowList] = useState(false);
 
-  const dispatch = useAppDispatch();
+  
   //const posts = useAppSelector((state) => state.posts.list);
   const deletedItems = useAppSelector((state) => state.deletedItems);
   const posts = useAppSelector(selectPosts);
-  const { qty, status, error } = useAppSelector(selectPostsInfo);
+  const { status, error } = useAppSelector(selectPostsInfo);
 
   const visiblePosts = posts.filter((post) => {
     return !deletedItems.includes(post.id);
@@ -31,11 +31,7 @@ export const News: React.FC = () => {
     }
   };
 
-  useEffect(() => {
-    if (!qty) {
-      dispatch(loadPosts());
-    }
-  }, [qty, dispatch]);
+  
 
   return (
     <>
@@ -44,7 +40,7 @@ export const News: React.FC = () => {
       {status === "received" && (
         <Grid justifyContent="center" container spacing={2} sx={{ mt: "45px" }}>
           {visiblePosts.slice(0, numberOfitemsShown).map((post) => (
-            <PostCard key={post.id} post={post} />
+            <PostCard key={post.id} post={post} home={false} />
           ))}
           <Grid item lg={12} sx={{display:"flex", justifyContent:"center"}} >
             <Button
